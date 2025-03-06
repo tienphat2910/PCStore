cbc063ada37ceb06705affef9922cde318b33d29
Nguyen Tien Phat <imissu3403@gmail.com>
2025-03-06 22:33:59 +0700

    them icon

--- client/src/constants/icons.js
+++ client/src/constants/icons.js
@@ -1,11 +1,17 @@
 import Message from "../assets/icons/message.svg";
 import Next from "../assets/icons/next.svg";
 import Previous from "../assets/icons/previous.svg";
+import Saving from "../assets/icons/Saving.png";
+import Account from "../assets/icons/Account.png";
+import Support from "../assets/icons/Support.png";
 
 const Icon = {
   Message,
   Next,
   Previous,
+  Saving,
+  Account,
+  Support,
 };
 
 export default Icon;
