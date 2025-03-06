6ae67f6298ab12441e298abcc10e8814056a284d
Nguyen Tien Phat <imissu3403@gmail.com>
2025-03-06 22:39:11 +0700

    doi duong dan anh

--- client/src/components/ProductDisplay/ProductFeatures.jsx
+++ client/src/components/ProductDisplay/ProductFeatures.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-
+import Icon from "../../constants/icons";
 const FeatureCard = ({ imageSrc, title, description }) => {
   return (
     <div className="card text-center border-0" style={{ backgroundColor: 'transparent' }}>
@@ -27,17 +27,17 @@ FeatureCard.propTypes = {
 const ProductFeatures = () => {
   const features = [
     {
-      imageSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/ba5da554e8aff6d82e10fc64978e48b8aaf1c3a432b77df19b656b018581ade1?placeholderIfAbsent=true&apiKey=1a2630dba26c44fe94fe53d5e705e42a",
+      imageSrc: Icon.Support,
       title: "Chế độ bảo hành",
       description: "Bảo hành tại chỗ lên đến 3 năm."
     },
     {
-      imageSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/c5542d98b15c467aba53e039deeba0db090cdd5da26f54b34ab4eda2e20b269a?placeholderIfAbsent=true&apiKey=1a2630dba26c44fe94fe53d5e705e42a",
+      imageSrc: Icon.Account,
       title: "Ưu đãi thành viên",
       description: "Với mức giảm giá lớn, giao hàng miễn phí và kỹ thuật viên hỗ trợ tận tình."
     },
     {
-      imageSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/d82549d7dc7b81b0dd9c49f9a35801a23a8a81a14112f97d0d38ce705946d9cf?placeholderIfAbsent=true&apiKey=1a2630dba26c44fe94fe53d5e705e42a",
+      imageSrc: Icon.Saving,
       title: "Tiết kiệm",
       description: "Giảm giá lên đến 70% cho Sản phẩm mới, bạn có thể chắc chắn về mức giá tốt nhất."
     }
